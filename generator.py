59f9e5dd599a8b5d063e41a3ab8ccf721881a57e
Marcvs101 <marcocuoci@gmail.com>
2023-04-26 18:20:40 +0200

    Generator is now functional

--- generator.py
+++ generator.py
@@ -29,18 +29,51 @@ os_dictionary = json.loads(f.read())
 f.close()
 
 f = open(file="dockerfile_resources/ubuntu_applications.json",mode="r",encoding="utf-8")
-os_dictionary = json.loads(f.read())
+application_dictionary = json.loads(f.read())
 f.close()
+
+
+
+# sanity check
+if SERVICES_PER_HOST_NUMBER >= len(application_dictionary["application_list"]):
+    SERVICES_PER_HOST_NUMBER = len(application_dictionary["application_list"])
+
+
+# assign operating system
+host_to_operating_system = dict()
+host_to_operating_system_id = dict()
+for host_number in range(HOST_NUMBER):
+    host_id = "host_"+str(host_number)
+
+    chosen_os = os_dictionary
+    chosen_release = random.choice(list(os_dictionary["release_map"].keys()))
+    # TODO
+    # make the os a list and allow for random selection
+
+    host_to_operating_system_id[host_id] = chosen_os["id"]
+
+    osdict = dict()
+    osdict["id"] = chosen_os["id"]
+    osdict["name"] = chosen_os["name"]
+    osdict["string"] = chosen_os["string"].replace(chosen_os["release_keyword"],chosen_os["release_map"][chosen_release])
+
+    host_to_operating_system[host_id] = osdict
+    
 
 
 # initialize host_to_service
 host_to_service = dict()
-for host_number in range(HOST_NUMBER):
-    host_id = "host_"+str(host_number)
-    host_to_service[host_id] = set()
+for host_id in host_to_operating_system:
+    host_to_service[host_id] = list()
+
+    selectable_services = list(application_dictionary["application_list"])
+    # TODO have different list for different OS
 
     for service_number in range(SERVICES_PER_HOST_NUMBER):
-        pass
+        chosen_service = random.choice(selectable_services)
+        selectable_services.remove(chosen_service)
+        host_to_service[host_id].append(chosen_service)
+
 
 
 # assign network
@@ -61,6 +94,8 @@ for host_id in host_to_service:
     host_to_network_address[host_id] = network_address
     counter = counter + 1
 
+
+
 # output
 #"""
 # cleanup
@@ -71,15 +106,37 @@ else:
     os.mkdir(OUTPUT_DIR)
 
 # dockerfile
-for host in host_to_service:
-    os.mkdir(OUTPUT_DIR+host)
-    f = open(file=OUTPUT_DIR+host+"/Dockerfile",mode="w",encoding="utf-8")
+for host_id in host_to_service:
+    os.mkdir(OUTPUT_DIR+host_id)
+    f = open(file=OUTPUT_DIR+host_id+"/Dockerfile",mode="w",encoding="utf-8")
+
+    # # operating system
+    f.write(host_to_operating_system[host_id]["string"])
+    f.write("\n\n")
+
+    # # applications
+    for application in host_to_service[host_id]:
+        f.write(application["string"])
+        f.write("\n\n")
+
+    # # shell script to start everything
+    f.write("# Create runfile\n")
+    f.write("RUN touch startup.sh\n")
+    for application in host_to_service[host_id]:
+        f.write("RUN echo "+application["shell_command"]+" & > startup.sh")
+        f.write("\n")
+    f.write("\n")
+
+    # # final command
+    f.write("# Run the circus\n")
+    f.write('CMD ["./startup.sh"]\n')
+
     f.close()
 
 # compose
 f = open(file=OUTPUT_DIR+"docker-compose.yml",mode="w",encoding="utf-8")
 f.write("version: '2'\n\n")
-
+# # scanner
 f.write("services:\n")
 f.write("  scanner:\n")
 f.write("    image: tenableofficial/nessus:latest\n")
@@ -91,6 +148,7 @@ f.write("      "+NETWORK_NAME+":\n")
 f.write("        ipv4_address: "+SCANNER_ADDRESS+"\n")
 f.write("\n")
 
+# # hosts
 for host_id in host_to_service:
     f.write("  "+host_id+":\n")
     f.write("    build: "+host_id+"\n")
@@ -100,6 +158,7 @@ for host_id in host_to_service:
     f.write("        ipv4_address: "+host_to_network_address[host_id]+"\n")
     f.write("\n")
 
+# # network
 f.write("networks:\n")
 f.write("  "+NETWORK_NAME+":\n")
 f.write("    driver: bridge\n")
@@ -108,5 +167,31 @@ f.write("      driver: default\n")
 f.write("      config:\n")
 f.write("       - subnet: "+NETWORK+"\n")
 f.write("         gateway: "+host_to_network_address["gateway"]+"\n")
+f.close()
+
+# inventory getters for windows
+f = open(file=OUTPUT_DIR+"windows_get_raw_device_inventory.bat",mode="w",encoding="utf-8")
+f.write("@echo off\n\n")
+for host_id in host_to_service:
+    f.write("docker sbom "+host_id+" > "+host_id+"/raw_device_inventory.txt\n")
+f.close()
+
+f = open(file=OUTPUT_DIR+"windows_get_raw_vulnerability_inventory.bat",mode="w",encoding="utf-8")
+f.write("@echo off\n\n")
+for host_id in host_to_service:
+    f.write("docker scout cves "+host_id+" -o "+host_id+"/raw_vulnerability_inventory.txt\n")
+f.close()
+
+# inventory getters for linux
+f = open(file=OUTPUT_DIR+"linux_get_raw_device_inventory.sh",mode="w",encoding="utf-8")
+f.write("#!/bin/bash\n\n")
+for host_id in host_to_service:
+    f.write("docker sbom "+host_id+" > "+host_id+"/raw_device_inventory.txt\n")
+f.close()
+
+f = open(file=OUTPUT_DIR+"linux_get_raw_vulnerability_inventory.sh",mode="w",encoding="utf-8")
+f.write("#!/bin/bash\n\n")
+for host_id in host_to_service:
+    f.write("docker scout cves "+host_id+" -o "+host_id+"/raw_vulnerability_inventory.txt\n")
 f.close()
 #"""
